b452580a60bd8913a577d162792386c908ad160e
Romina <rominahelou@gmail.com>
2023-06-03 22:29:49 +0200

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from '../views/Home.vue'
+import MealList from '../views/MealList.vue'
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/letter/:letter",
+    name: "byLetter",
+    component: MealList,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col p-8 ">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+        <div class="flex gap-2 justify-center mt-2">
+            <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+</script>
